30635791d241a7a636883bddacd8e50a5de710c5
IVIosab <mosab.f.r@gmail.com>
2023-03-23 01:20:50 +0300

    first commit

---
+++ src/lib/Header.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+</script>
+
+<header>
+	<h1>Mosab "IVIosab" Mohamed</h1>
+	<nav>
+		<ul>
+			<li><a href="{base}/">Home</a></li>
+			<li><a href="{base}/about">About</a></li>
+			<li><a href="{base}/projects">Projects</a></li>
+			<li><a href="{base}/comic">Comic</a></li>
+		</ul>
+	</nav>
+</header>
+
+<style>
+	header {
+		box-sizing: border-box;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 1rem;
+		background-color: #333;
+		color: #fff;
+	}
+	nav ul {
+		display: flex;
+		list-style: none;
+	}
+
+	nav ul li {
+		margin: 0 1rem;
+	}
+
+	nav a {
+		color: #fff;
+	}
+
+	a {
+		text-decoration: none;
+		color: black;
+	}
+
+	h1 {
+		padding-left: 10px;
+	}
+</style>
