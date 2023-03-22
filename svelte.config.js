7a865ab08a26226506f37483668e77fc2e39d7ad
IVIosab <mosab.f.r@gmail.com>
2023-03-23 01:42:59 +0300

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
-const dev = process.argv.includes('dev');
 
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -10,15 +10,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false,
-			strict: true
-		}),
+		adapter: adapter(),
 		paths: {
 			base: dev ? '' : '/IVIosab.github.io'
 		}
